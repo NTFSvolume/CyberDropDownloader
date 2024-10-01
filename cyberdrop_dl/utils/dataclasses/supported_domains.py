a258d66c72d5e9340f8e9261745b827ba25915dc
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2024-09-30 22:08:06 -0500

    feat: Add simpcity to supported hosts

--- cyberdrop_dl/utils/dataclasses/supported_domains.py
+++ cyberdrop_dl/utils/dataclasses/supported_domains.py
@@ -5,18 +5,18 @@ from typing import ClassVar, Tuple, List
 @dataclass
 class SupportedDomains:
     """The allows options for domains to skip when scraping and mappings"""
-    supported_hosts: ClassVar[Tuple[str, ...]] = ("bunkr", "bunkrr", "celebforum", "coomer", "cyberdrop", "cyberfile",
-                                                "e-hentai", "erome", "fapello", "f95zone", "gofile", "hotpic",
-                                                "ibb.co", "imageban", "imgbox", "imgur", "img.kiwi", "jpg.church",
-                                                "jpg.homes", "jpg.fish", "jpg.fishing", "jpg.pet", "jpeg.pet",
-                                                "jpg1.su", "jpg2.su", "jpg3.su", "jpg4.su", "jpg5.su", "host.church",
-                                                "kemono",
-                                                "leakedmodels", "mediafire", "nudostar.com", "nudostar.tv",
-                                                "omegascans", "pimpandhost", "pixeldrain", "postimg", "realbooru",
-                                                "reddit", "redd.it", "redgifs", "rule34.xxx", "rule34.xyz",
-                                                "rule34vault", "saint",
-                                                "scrolller", "simpcity", "socialmediagirls", "toonily", "xbunker",
-                                                "xbunkr")
+    supported_hosts: ClassVar[Tuple[str, ...]] = ("simpcity","bunkr", "bunkrr", "celebforum", "coomer", "cyberdrop", "cyberfile",
+                                                  "e-hentai", "erome", "fapello", "f95zone", "gofile", "hotpic",
+                                                  "ibb.co", "imageban", "imgbox", "imgur", "img.kiwi", "jpg.church",
+                                                  "jpg.homes", "jpg.fish", "jpg.fishing", "jpg.pet", "jpeg.pet",
+                                                  "jpg1.su", "jpg2.su", "jpg3.su", "jpg4.su", "jpg5.su", "host.church",
+                                                  "kemono",
+                                                  "leakedmodels", "mediafire", "nudostar.com", "nudostar.tv",
+                                                  "omegascans", "pimpandhost", "pixeldrain", "postimg", "realbooru",
+                                                  "reddit", "redd.it", "redgifs", "rule34.xxx", "rule34.xyz",
+                                                  "rule34vault", "saint",
+                                                  "scrolller", "simpcity", "socialmediagirls", "toonily", "xbunker",
+                                                  "xbunkr")
 
     supported_forums: ClassVar[Tuple[str, ...]] = ("celebforum.to", "f95zone.to", "leakedmodels.com", "nudostar.com",
                                                 "simpcity.su", "forums.socialmediagirls.com", "xbunker.nu")
