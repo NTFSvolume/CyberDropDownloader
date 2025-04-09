b26035eb7cc60a9d89bf181c425055a68f8015a8
NTFSvolume <172021377+NTFSvolume@users.noreply.github.com>
2025-04-09 16:19:59 -0500

    feat: auto get user-agent from browser

--- cyberdrop_dl/utils/cookie_management.py
+++ cyberdrop_dl/utils/cookie_management.py
@@ -1,16 +1,22 @@
 from __future__ import annotations
 
+import asyncio
 import os
+import socket
+import webbrowser
 from functools import wraps
 from http.cookiejar import MozillaCookieJar
 from textwrap import dedent
 from typing import TYPE_CHECKING
 
 import browser_cookie3
+from multidict import CIMultiDict
 from rich.console import Console
 
+from cyberdrop_dl.utils import constants
+
 if TYPE_CHECKING:
-    from collections.abc import Callable
+    from collections.abc import Callable, Generator
 
     from cyberdrop_dl.managers.manager import Manager
 
@@ -98,10 +104,95 @@ def clear_cookies(manager: Manager, domains: list[str] | None = None) -> None:
 
 def check_unsupported_browser(error: browser_cookie3.BrowserCookieError, extractor_name: str) -> None:
     msg = str(error)
-    if is_decrypt_error(msg) and extractor_name in CHROMIUM_BROWSERS and os.name == "nt":
-        msg = f"Cookie extraction from {extractor_name.capitalize()} is not supported on Windows - {msg}"
+    is_decrypt_error = "Unable to get key for cookie decryption" in msg
+    if is_decrypt_error and extractor_name in CHROMIUM_BROWSERS and os.name == "nt":
+        msg = f"Cookie extraction from {extractor_name.capitalize()} is not supported on Windows. Use a Firefox based browser - {msg}"
         raise UnsupportedBrowserError(msg)
 
 
-def is_decrypt_error(error_as_str: str) -> bool:
-    return "Unable to get key for cookie decryption" in error_as_str
+async def get_browser_user_agent(browser: str | None = None) -> str | None:
+    """Get User-Agent header from browser.
+
+    If browser is `None`, the default browser in the OS will be used
+    or the specified browser is not available
+    If there any errors , returns `None`"""
+
+    web_browser = webbrowser.get(browser)
+    loop = asyncio.get_running_loop()
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # IPV4, TCP
+    server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    server.bind(("127.0.0.1", 0))
+    server.listen(1)
+    host, port = server.getsockname()
+    url = f"http://{host}:{port}/cyberdrop-dl-user-agent-check"
+    success = await asyncio.to_thread(web_browser.open, url, autoraise=False)
+    if not success:
+        return
+
+    try:
+        client, _ = await asyncio.wait_for(loop.sock_accept(server), 10)
+    except TimeoutError:
+        return
+
+    server.close()
+    response_data = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n"
+    response_data += """
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>CDL User-Agent</title>
+        </head>
+        <body>
+            <h2>Cyberdrop-DL will use this user-agent:</h2>
+    """
+
+    try:
+        headers, _ = await get_headers_and_body(client)
+        user_agent = headers["user-agent"]
+        date_str = f"<h2>Startup time:</h2> {constants.STARTUP_TIME.isoformat()}"
+        response_data += f"<p>{user_agent}</p><p>{date_str}</p></body></html>"
+        await asyncio.to_thread(client.sendall, response_data.encode("utf-8"))
+        return user_agent
+    finally:
+        client.close()
+
+
+async def get_headers_and_body(socket: socket.socket) -> tuple[CIMultiDict, bytes]:
+    """Reads data from a socket in chunks and extracts the HTTP headers + body."""
+
+    response_data = b""
+    body_start = -1
+    chunk_size = 512
+    loop = asyncio.get_running_loop()
+
+    while True:
+        chunk = await loop.sock_recv(socket, chunk_size)
+        if not chunk:
+            if not response_data:
+                raise ConnectionError("No data received")
+            break
+
+        response_data += chunk
+        body_start = response_data.find(b"\r\n\r\n")
+        if body_start != -1:
+            break
+
+    if body_start == -1:
+        raise ConnectionError("Incomplete headers received")
+
+    headers_bytes, body_bytes = response_data[:body_start], response_data[body_start + 4 :]
+
+    def gen_header_pairs() -> Generator[tuple[str, str]]:
+        for line in headers_bytes.decode("utf-8").splitlines():
+            if not line or ":" not in line:
+                continue
+            name, value = line.split(":", 1)
+            yield name.strip(), value.strip()
+
+    return CIMultiDict(gen_header_pairs()), body_bytes
+
+
+if __name__ == "__main__":
+    print(asyncio.run(get_browser_user_agent()))  # noqa: T201
